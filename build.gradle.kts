f24f810e108eb086c2960d090f2d2b5da9d6a022
OhMyCatile <ohmycatile@gmail.com>
2023-11-09 18:49:13 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+}
+
+group = "io.evil"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
